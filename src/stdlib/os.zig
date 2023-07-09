74e08b438181ba3c6079c998a7f1b22f29ba03b3
Palash Bauri <palashbauri1@gmail.com>
2023-07-09 11:29:15 +0530

    simplified os name
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/stdlib/os.zig
+++ src/stdlib/os.zig
@@ -1,27 +1,34 @@
-const std = @import("std");
 const value = @import("../value.zig");
 const Gc = @import("../gc.zig").Gc;
 const PValue = value.PValue;
 const utils = @import("../utils.zig");
 const stdlib = @import("stdlib.zig");
 const msl = stdlib.msl;
+const builtin = @import("builtin");
 
-
-pub fn os_Name(gc : *Gc , argc: u8, values: []PValue) PValue {
+pub fn os_Name(gc: *Gc, argc: u8, values: []PValue) PValue {
     _ = values;
     _ = argc;
     const v =
-        gc.copyStringU8("linux", 5) orelse {
-            return PValue.makeNil();
-        };
+        gc.copyStringU8(
+        switch (builtin.target.os.tag) {
+            .windows => "windows",
+            .linux => "linux",
+            else => "unknown",
+        },
+        0,
+    ) orelse {
+        return PValue.makeNil();
+    };
+
     return PValue.makeObj(v.parent());
 }
 
-pub fn _get_os() []msl{
+
+pub fn _get_os() []msl {
     const items = [_]stdlib.msl{
-       msl.m(&[_]u32{'n' , 'a' , 'm' , 'e'}, os_Name),
+        msl.m(&[_]u32{ 'n', 'a', 'm', 'e' }, os_Name),
     };
 
     return &items;
 }
-
