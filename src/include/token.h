fc0eaee5ed2a5727b30f1adc0ad88f11842d1ad4
Palash Bauri <palashbauri1@gmail.com>
2025-10-16 19:19:03 +0530

    finished : well commented headers
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/include/token.h
+++ src/include/token.h
@@ -8,76 +8,149 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
+// Token Types
 typedef enum TokenType{
+	// Token `(`
 	T_LEFT_PAREN,
+	
+	// Token `)`
 	T_RIGHT_PAREN,
+
+	// Token `{`
 	T_LEFT_BRACE,
+
+	// Token `}`
 	T_RIGHT_BRACE,
+
+	// Token `[`
 	T_LS_BRACKET,
+
+	// Token `]`
 	T_RS_BRACKET,
+
+	// Token `,`
 	T_COMMA,
+	// Token `.`
 	T_DOT,
+	// Token `+`
 	T_PLUS,
+	// Token `-`
 	T_MINUS,
+	// Token `:`
 	T_COLON,
+	// Token `<Undecided>`
 	T_MOD,
+	// Token `;`
 	T_SEMICOLON,
+	// Token `/`
 	T_SLASH,
+	// Token `*`
 	T_ASTR,
 
+	// Token `!`
 	T_BANG,
+	// Token `!=`
 	T_BANG_EQ,
+	// Token `=`
 	T_EQ,
+	// Token `==`
 	T_EQEQ,
+	// Token `>`
 	T_GT,
+	// Token `>=`
 	T_GTE,
+	// Token `<`
 	T_LT,
+	// Token `<=`
 	T_LTE,
 
+	// Token : Identifier `myvar = 100`
 	T_IDENT,
+	// Token : String `"<str>"`
 	T_STR,
+	// Token : Number `100.00`
 	T_NUM,
 
+	// Token : Keyword `let/dhori`
 	T_LET,
+	// Token : Keyword `and/ebong`
 	T_AND,
+	// Token : Keyword `or/ba`
 	T_OR,
+	// Token : Keyword `func/kaj`
 	T_FUNC,
+	// Token : Keyword `if/jodi`
 	T_IF,
+	// Token : Keyword `then/tahole`
 	T_THEN,
+	// Token : Keyword `else/nahole`
 	T_ELSE,
+	// Token : Keyword `end/sesh`
 	T_END,
+	// Token : Keyword `while/jotokhon`
 	T_WHILE,
+	// Token : Keyword `do/koro`
 	T_DO,
+	// Token : Keyword `break/bhango`
 	T_BREAK,
+	// Token : Keyword `nil`
 	T_NIL,
+	// Token : Keyword `true/sotti`
 	T_TRUE,
+	// Token : Keyword `false/mittha`
 	T_FALSE,
+	// Token : Keyword `return/ferot`
 	T_RETURN,
+	// Token : Keyword `import/anoyon`
 	T_IMPORT,
+	// Token : Keyword `panic/golmal`
 	T_PANIC,
+	// Token : Keyword `len/ayoton`
 	T_LEN,
+	// Token : Keyword `print/print`
 	T_PRINT,
 
+	// Token : End of File
 	T_EOF
 }TokenType;
 
+// Get Token Type as String
 char * TokTypeToStr(TokenType type);
+// Print Token Type
 void PrintTokType(TokenType type);
+// Is Token Double character?
 bool IsDoubleTok(TokenType type);
 
+// Token Object
 typedef struct Token{
+	// Token Type
 	TokenType type;
+	// Optional Lexeme : Referenced from source directly using string indexing.
+	// Only used for Keywords, identifiers, strings, numbers.
+	// Can be NULL
 	char * lexeme;
+	// Line number
 	long line;
+	// Column number. Distance from first character of the line
 	long col;
+	// length of the token lexeme, if lexeme is optional depends of how many 
+	// characters the token contains need. For example, T_EQ is 1, T_EQEQ is 2
 	long len;
+	// Hash for the token lexeme, expect for Keywords, identifiers, strings,
+	// numbers it will be 0 (zero)
 	uint32_t hash;
 }Token;
 
+// Create an empty token.
+// `type` = Token type
 Token * NewToken(TokenType type);
+// Free the token object
 void FreeToken(Token * token);
+// Set token lexeme. Calculate and set hash.
 bool SetTokenLexeme(Token * token, char * str);
+// Print Token
 void PrintToken(const Token * token);
+// Print operator type tokens. Special formatting is done as lexeme is NULL.
 void PrintOpToken(const Token * token);
 
 #ifdef __cplusplus
--- src/include/ustring.h
+++ src/include/ustring.h
@@ -1,31 +1,67 @@
 #ifndef USTRING_H
 #define USTRING_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+// How many codepoints the Iterator can peek
 #define UITER_PEEK_BUFFER_SIZE 3
+// Placeholder return of invalid codepoints
 #define UITER_INVALID_CODEPOINT 0xFFFD
 
+// Unicode Iterator Object
 typedef struct UIter{
+	// Raw UTF-8 encoded string. Reference to source from Lexer object
 	const char * str;
+	// How many characters is in `str`. 
 	size_t len;
+	// Current position of iterator
 	size_t pos;
 
+	// Stack allocated peek buffer.
+	// Index 0 is next codepoint.
 	uint32_t peekBuf[UITER_PEEK_BUFFER_SIZE];
+
+	// How many peekable codepoint is available
 	int peekCount;
 }UIter;
 
+// New Unicode string iterator object
+// `text` = UTF-8 encoded string
 UIter * NewUIterator(const char * text);
+
+// Free Iterator object
 void FreeUIterator(UIter * iter);
+
+// Is the iterator at the end of string
 bool UIterIsEnd(const UIter * iter);
+
+// Peek the next codepoint
+// `offset` = returns `current codepoint index + offset` 
 uint32_t UIterPeek(const UIter * iter, int offset);
+
+// Advance the iterator
 void UIterAdvance(UIter * iter);
+
+// Advance the iterator and return the just passed codepoint
 uint32_t UIterNext(UIter * it);
 
-
+// Convert the UTF-32 encoded codepoint to UTF-8 encoded character array
+// `value` = UTF-32 encoded codepoint
+// `result` = buffer to write the UTF-8 encoded bytes
 void U32ToU8(uint32_t value, uint8_t result[4]);
+
+// Debug/Print the peek buffer
 void DebugPeekBuffer(const UIter * it);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
