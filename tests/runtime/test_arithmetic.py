3e8a5569267594704874d555a3d1c4ee833b2baa
Palash Bauri <palashbauri1@gmail.com>
2025-12-14 19:33:24 +0530

    add reminder for bom test file handling
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- tests/runtime/test_arithmetic.py
+++ tests/runtime/test_arithmetic.py
@@ -6,6 +6,7 @@ class TestBasicArithmetic(testcore.PanktiTestCase):
     def test_arithmetic_precedence(self):
         self.golden("arithmetic")
     def test_arithmetic_with_bom_in_file(self):
+        # Never edit this file on *nix systems, as it may remove the BOM.
         self.golden("arithmetic_bom")
 
 
