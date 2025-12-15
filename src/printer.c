2db0a2a4f59f3d4c6e75acd6b6cae5b22f76c026
Palash Bauri <palashbauri1@gmail.com>
2025-12-15 20:05:12 +0530

    nix impl for panprint functions
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/printer.c
+++ src/printer.c
@@ -1,1 +1,29 @@
 #include "include/printer.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+#if defined (PANKTI_OS_WIN)
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+int PanPrint(const char *format, ...){
+	va_list args;
+	va_start(args, format);
+	int result = vprintf(format, args);
+	va_end(args);
+	return result;
+}
+
+int PanFPrint(void *stream, const char *format, ...){
+	va_list args;
+	va_start(args, format);
+	int result = vfprintf((FILE*)stream, format, args);
+	va_end(args);
+	return result;
+}
+
+int PanVPrint(const char *format, va_list args){
+	return vprintf(format, args);
+}
