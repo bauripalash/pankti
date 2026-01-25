2f790a2c6d5e1b4341840ee53ba3704b0396c1f5
Palash Bauri <palashbauri1@gmail.com>
2026-01-25 18:21:26 +0530

    revert a example
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- src/external/stb/stb_ds_impl.c
+++ src/external/stb/stb_ds_impl.c
@@ -1,5 +1,8 @@
 // #define STBDS_SIPHASH_2_4
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
+
+#if defined(HAS_GFX_SUPPORT) && !defined(NO_GFX_SUPPORT)
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
+#endif
