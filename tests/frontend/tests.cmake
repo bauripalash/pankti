25eacbb3acd09d96017605a36491511b2787c651
Palash Bauri <palashbauri1@gmail.com>
2026-06-08 10:29:39 +0530

    add copyright info in top license header
    Signed-off-by: Palash Bauri <palashbauri1@gmail.com>

--- tests/frontend/tests.cmake
+++ tests/frontend/tests.cmake
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 Palash Bauri
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 set(PANKTI_NATIVE_TEST_FILES
   "${CMAKE_CURRENT_LIST_DIR}/test_main.c"
   "${CMAKE_CURRENT_LIST_DIR}/test_lexer.c"
